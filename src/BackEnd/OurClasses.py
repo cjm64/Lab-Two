690dd709a39887800e10f0c3eae3203abee8adc4
Jake Vickerd <jakevick@buffalo.edu>
2019-03-13 10:20:26 -0400

    Hola

---
+++ src/BackEnd/OurClasses.py
@@ -0,0 +1,26 @@
+"""
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+"""
+
+
+class thing:
+
+    sizeX = None
+    sizeY = None
+
+    direction = None # 1 or -1
+
+    # we can add like an image reference here too like: image = "anImage.png"
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
