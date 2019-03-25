6897171fbe091dbf19c201fb78e2840b6c429945
Jake Vickerd <jakevick@buffalo.edu>
2019-03-25 11:02:13 -0400

    Hola

--- src/BackEnd/Main.py
+++ src/BackEnd/Main.py
@@ -1,16 +1,25 @@
 import BackEnd.JakesFunctions
 import BackEnd.OurClasses
 import time
-# import sqlite3
+import sqlite3
 
 breakLoop = False
 currentTime = 0.0
 
+conn = sqlite3.connect('data.db')
+cur = conn.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS players (playerName, sizeX, sizeY, x, y)')
+cur.execute('INSERT INTO players VALUES ("Jerry", 20, 20, 54, 36)')
+
 # code in this block will execute every 0.1 seconds (not including runtime)
 while True:
+
+    print(cur.execute('SELECT * FROM players WHERE playerName=?',("Jerry",)))
 
     if breakLoop:
         break
 
-    time.sleep(0.1)
-    currentTime += 0.1
+    time.sleep(1)
+    currentTime += 1
+
+conn.close()
