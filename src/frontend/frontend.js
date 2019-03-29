b8a86c46648606782a1460e9c60e44f3ee47266f
cjm64 <cjm64@buffalo.edu>
2019-03-29 10:05:50 -0400

    Merge branch 'master' of C:\Users\maste\IdeaProjects\Lab-Two with conflicts.

--- src/frontend/frontend.js
+++ src/frontend/frontend.js
@@ -2,15 +2,19 @@ function startGame() {
     new Phaser.Game(config);
 } //Phaser startgame
 
+var name = prompt("Please enter your name", "Username");
+
 var jason = {
+    'username' : name,
     'vertical' : 0,
     'horizontal' : 0
 } //Initializes the dictionary that will be used to send JSON to server.
 
+
 var config = {
     "type" : Phaser.AUTO,
-    "width" : 1600,
-    "height" : 1600,
+    "width" : 800,
+    "height" : 800,
     physics: {
         default: 'arcade',
         arcade: {
@@ -27,8 +31,8 @@ var config = {
             healthpoints: null,
             reticle: null,
             moveKeys: null,
-            playerBullets: null,
-            enemyBullets: null,
+            playerProjectiles: null,
+            enemyProjectiles: null,
             time: 0
         }
     }
@@ -46,56 +50,53 @@ var fire = 0;
 var board;
 var board1;
 var board2;
-var movecam = false;
-//More Phaser Stuff
+var imm = -300
+//More Phaser Stuff and variables
 
 
 function preload(){
     this.load.image('Player', 'assets/player.png');
     this.load.image('Shot', 'assets/shot.png');
     this.load.image('Enemy', 'assets/enemy.png');
-    this.load.image('EnemyBullet', 'assets/bullet.png');
+    this.load.image('EnemyProjectile', 'assets/bullet.png');
     this.load.image('Background', 'assets/bg.png');
-}//More Phaser Stuff PT. 3
+}//More Phaser Stuff PT. 3. Loads images for sprites
 
 function create () {
 
-    playerBullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true });
-    enemyBullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true });
-    //Creates classes for player bullets and enemy bullets, so that they don't shoot themselves. Unsure of how this might work in server, but probably easily doable.
 
-    for (y = 0; y < 2; y++)
-    {
-        for (x = 0; x < 2; x++)
-        {
-            this.add.image(400 * x, 400 * y, 'Background').setOrigin(0).setAlpha(0.75);
-        }
-    }
-    //this.add.image(400, 400, 'Background');
+
+    playerProjectiles = this.physics.add.group({ classType: Projectile, runChildUpdate: true });
+    enemyProjectiles = this.physics.add.group({ classType: Projectile, runChildUpdate: true });
+    //Creates classes for player projectiles and enemy projectiles, so that they don't shoot themselves. Unsure of how this might work in server, but probably easily doable.
+
+
+    this.add.image(400, 400, 'Background');
     //Background.jpg
 
-    player = this.physics.add.sprite(400, 400, 'Player');
+    player = this.physics.add.sprite(400, 600, 'Player');
     //Creates Player
 
     player.setCollideWorldBounds(true);
     //Phaser function so that player cannot bypass world bounds
 
     this.input.on('pointerdown', function (pointer, time, lastFired) {
-        //Only shoots if the player has not fired in the past 20 frames.
-        if (fire > 20) {
+        //Only shoots if the player has not fired a shot in the past 20 frames
+         if (fire > 20) {
             fire = 0
             if (player.active === false)
                 return;
 
-            var bullet = playerBullets.get().setActive(true).setVisible(true);
+            var projectile = playerProjectiles.get().setActive(true).setVisible(true);
 
-            if (bullet) {
-                bullet.fire(player, pointer);
-                //Bullet moves from shooter(player) to pointer(cursor).
-                this.physics.add.collider(enemy1, bullet, enemyHitCallback);
+            if (projectile) {
+                projectile.shoot(player, pointer);
+                //Projectile moves from shooter(player) to pointer(cursor).
+                this.physics.add.collider(enemy1, projectile, enemyHitFunction);
             }
         }
-    }, this); //Shoots a bullet when the mouse is clicked.
+
+    }, this); //Shoots a projectile when the mouse is clicked.
 
     enemy1 = this.physics.add.sprite(100, 100, 'Enemy');
     //Creates an enemy
@@ -117,70 +118,30 @@ function create () {
     hpText = this.add.text(16, 16, 'Health: 5', { fontSize: '32px', fill: '#000' });
     deathText = this.add.text(16, 116, 'Deaths: 0', { fontSize: '32px', fill: '#000' });
     board = this.add.text(550, 16, 'Top Players:', { fontSize: '32px', fill: '#000' });
-    board1 = this.add.text(550, 66, 'Player: 0', { fontSize: '32px', fill: '#000' });
+    board1 = this.add.text(550, 66, name + ": 0", { fontSize: '32px', fill: '#000' });
     board2 = this.add.text(550, 116, 'Enemy: 0', { fontSize: '32px', fill: '#000' });
 
-    this.cameras.main.startFollow(player, true);
-    this.cameras.main.setDeadzone(100, 50);
-    this.cameras.main.setZoom(0.5);
-    this.input.on('pointerdown', function () {
-        moveCam = (moveCam) ? false: true;
-    });
+
 
 
 }
 
-function update(time, delta){
-    var cam = this.cameras.main;
+function update(time){
 
     //It's the update function.
 
     var cursors = this.input.keyboard.createCursorKeys();
-    if (movecam)
-    {
-        if (cursors.left.isDown)
-        {
-            cam.scrollX -= 4;
-        }
-        else if (cursors.right.isDown)
-        {
-            cam.scrollX += 4;
-        }
-
-        if (cursors.up.isDown)
-        {
-            cam.scrollY -= 4;
-        }
-        else if (cursors.down.isDown)
-        {
-            cam.scrollY += 4;
-        }
-    }
-    if (cursors.left.isDown) {
-        jason['horizontal'] = -1;
-    }
-    else if (cursors.right.isDown) {
-        jason['horizontal'] = 1;
-    }
-    else {
-        jason['horizontal'] = 0;
-    }
-    if (cursors.up.isDown) {
-        jason['vertical'] = -1;
-    }
-    else if (cursors.down.isDown) {
-        jason['vertical'] = 1;
-    }
-    else {
-        jason['vertical'] = 0;
-    }
-    if (cursors.up.isDown && cursors.down.isDown) {
-        jason['vertical'] = 0;
-    }
-    if (cursors.left.isDown && cursors.right.isDown) {
-        jason['horizontal'] = 0;
-    }
+    if (cursors.left.isDown) {jason['horizontal'] = -1;}
+    else if (cursors.right.isDown) {jason['horizontal'] = 1;}
+    else {jason['horizontal'] = 0;}
+    if (cursors.up.isDown) {jason['vertical'] = -1;}
+    else if (cursors.down.isDown) {jason['vertical'] = 1;}
+    else {jason['vertical'] = 0;}
+    if (cursors.up.isDown && cursors.down.isDown) {jason['vertical'] = 0;}
+    if (cursors.left.isDown && cursors.right.isDown) {jason['horizontal'] = 0;}
     // Above code sets variable "horizontal" and "vertical" in dictionary to 1 or 0 based on arrow key inputs
+
+
 
     var jay = JSON.stringify(jason);
     var par = JSON.parse(jay);
@@ -190,83 +151,84 @@ function update(time, delta){
     //Sets position for X and Y based on JSON, which is based on arrow key input.
     //Could be set to do velocity instead, but then players would be able to set own position in console
     //Using position instead of velocity the player can sort of bypass world bounds up to its center.
+    //Json (jay) = 'username': player's username 'horizontal' : 0 or 1, 'vertical' : 0 or 1
 
 
     enemyFire(enemy1, player, time, this);
-    //Enemy shoots a bullet whenever it's cooldown is over.
+    //Enemy shoots a projectile whenever it's cooldown is over.
 
     fire +=1
+
 
     return;
 }
 
 
 
-//Class for bullets. Handles how they act.
-var Bullet = new Phaser.Class({
+//Class for projectiles. Handles how they act.
+var Projectile = new Phaser.Class({
 
     Extends: Phaser.GameObjects.Image,
 
     initialize:
 
-    // Bullet Constructor
-        function Bullet (scene)
+    // Constructor for projectiles
+        function Projectile (scene)
         {
             Phaser.GameObjects.Image.call(this, scene, 0, 0, 'Shot');
-            this.speed = .5;
-            this.born = 0;
-            this.direction = 0;
+            this.speed = 7.5;
+            this.age = 0;
+            this.angle = 0;
             this.xSpeed = 0;
             this.ySpeed = 0;
-            this.setSize(15, 15, true);
+            this.setSize(16, 16, true);
         },
 
-    // Fires a bullet from the player to the reticle
-    fire: function (shooter, target)
+    //Fire function for projectile. Moves from object shooting to target
+    shoot: function (int, dest)
     {
-        this.setPosition(shooter.x, shooter.y); // Initial position
-        this.direction = Math.atan( (target.x-this.x) / (target.y-this.y));
+        // Initial position is set to the object shooting's position
+        this.x = int.x
+        this.y = int.y
 
-        // Calculate X and y velocity of bullet to moves it from shooter to target
-        if (target.y >= this.y)
+        this.angle = Math.atan( (dest.x-this.x) / (dest.y-this.y));
+
+        // Calculate the xSpeed and ySpeed of projectile to moves it from initial position to target
+        this.xSpeed = this.speed*Math.sin(this.angle);
+        this.ySpeed = this.speed*Math.cos(this.angle);
+
+        if (dest.y < this.y)
         {
-            this.xSpeed = this.speed*Math.sin(this.direction);
-            this.ySpeed = this.speed*Math.cos(this.direction);
-        }
-        else
-        {
-            this.xSpeed = -this.speed*Math.sin(this.direction);
-            this.ySpeed = -this.speed*Math.cos(this.direction);
+            this.xSpeed = -this.xSpeed
+            this.ySpeed = -this.ySpeed
         }
 
-        this.rotation = shooter.rotation; // angle bullet with shooters rotation
-        this.born = 0; // Time since new bullet spawned
+        this.age = 0; // Age of projectile
     },
 
-    // Updates the position of the bullet each cycle
-    update: function (time, delta) {
-        this.x += this.xSpeed * delta;
-        this.y += this.ySpeed * delta;
-        this.born += delta;
-        if (this.born > 1800) {
-            this.setActive(false);
-            this.setVisible(false);
-            //bullet hides and deactivates itself after 1800 frames
+    // Updates the position and age of the projectile each cycle
+    update: function () {
+        this.age += 1;
+        if (this.age > 120) {
+            this.destroy();
+            //projectile destroys intself after 120 frames
         }
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+
     }
 
 });
 
 
 //Example collision code for demonstration.
-function enemyHitCallback(enemyHit, bulletHit)
+function enemyHitFunction(enemyHit, projectileHit)
 {
     // Randomize speed and direction after getting shot
     enemyHit.setVelocity(Phaser.Math.Between(80, 200), Phaser.Math.Between(80, 200));
-    if (bulletHit.active === true && enemyHit.active === true)
+    if (projectileHit.active === true && enemyHit.active === true)
     {
         enemyHit.health = enemyHit.health - 1;
-        console.log("Enemy hp: ", enemyHit.health);
 
         // Kill enemy if health <= 0
         if (enemyHit.health <= 0)
@@ -278,27 +240,26 @@ function enemyHitCallback(enemyHit, bulletHit)
             scoreText.setText('Kills: ' + kills);
             if (kills > deaths){
                 board2.setText("Enemy: " + deaths)
-                board1.setText("Player: " + kills)
+                board1.setText(name + " : " + kills)
             }
             if (deaths > kills){
                 board1.setText("Enemy: " + deaths)
-                board2.setText("Player: " + kills)
+                board2.setText(name + " : " + kills)
             }
         }
 
-        // Destroy bullet
-        bulletHit.setActive(false).setVisible(false);
+        // Destroy projectile
+        projectileHit.destroy();
     }
 }
 
 //Example collision code for demonstration.
-function playerHitCallback(playerHit, bulletHit)
+function playerHitFunction(playerHit, projectileHit)
 {
     // Reduce health of player
-    if (bulletHit.active === true && playerHit.active === true)
+    if (projectileHit.active === true && playerHit.active === true)
     {
         playerHit.health = playerHit.health - 1;
-        console.log("Player hp: ", playerHit.health);
         hpText.setText('Health: ' + player.health)
 
         if (playerHit.health <= 0){
@@ -310,17 +271,17 @@ function playerHitCallback(playerHit, bulletHit)
             hpText.setText('Health: ' + player.health)
             if (deaths > kills){
                 board1.setText("Enemy: " + deaths)
-                board2.setText("Player: " + kills)
+                board2.setText(name + " :" + kills)
             }
             if (kills > deaths){
                 board2.setText("Enemy: " + deaths)
-                board1.setText("Player: " + kills)
+                board1.setText(name + " : " + kills)
             }
 
         }
 
-        // Destroy bullet
-        bulletHit.setActive(false).setVisible(false);
+        // Destroy projectile
+        projectileHit.destroy();
     }
 }
 
@@ -336,14 +297,14 @@ function enemyFire(enemy1, player, time, gameObject)
     {
         enemy1.lastFired = time;
 
-        // Get bullet from bullets group
-        var bullet = enemyBullets.get().setActive(true).setVisible(true);
+        // Get projectile from projectiles group
+        var projectile = enemyProjectiles.get().setActive(true).setVisible(true);
 
-        if (bullet)
+        if (projectile)
         {
-            bullet.fire(enemy1, player);
-            // Add collider between bullet and player
-            gameObject.physics.add.collider(player, bullet, playerHitCallback);
+            projectile.shoot(enemy1, player);
+            // Add collider between projectile and player
+            gameObject.physics.add.collider(player, projectile, playerHitFunction);
         }
     }
 }
