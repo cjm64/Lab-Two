388a2cf704002b65479e1cee5222dbd1ccd6e61b
ealopez2 <emlopez7711@gmai.com>
2019-04-01 08:42:59 -0400

    committed

--- src/frontend/tests.js
+++ src/frontend/tests.js
@@ -14,8 +14,7 @@ within = config.to.be.within(0, 800);
 
 describe('test collision', playerHitFunction(true, true), {
     it('returns the boolean of condition': {
-        var result = true;
-
-        assert.equal(result, true, 'collision');
-    });
-});
+        var: result = true,
+        assert:equal(result, true, 'collision'),
+}
+};
