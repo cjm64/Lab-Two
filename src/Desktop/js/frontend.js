9ba8d1d0405905b2c6829380e4551f1afa014c2b
JustinKozlowski <jjkozlow@buffalo.edu>
2019-05-03 10:05:52 -0400

    started working on making desktop and make shooting send json instead

--- src/Desktop/js/frontend.js
+++ src/Desktop/js/frontend.js
@@ -5,19 +5,55 @@ function startGame() {
 var name;
 var gameState;
 
+var WebSocketClient = require('websocket').client;
 
-var socket = io.connect({transports: ['websocket']});
+var client = new WebSocketClient();
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+    console.log('WebSocket Client Connected');
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function() {
+        console.log('echo-protocol Connection Closed');
+    });
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+        }
+    });
+
+    function sendNumber() {
+        if (connection.connected) {
+            var number = Math.round(Math.random() * 0xFFFFFF);
+            connection.sendUTF(number.toString());
+            setTimeout(sendNumber, 1000);
+        }
+    }
+    sendNumber();
+});
+
+client.connect('ws://localhost:8081/', 'echo-protocol');
+
+
+
+
+/*var socket = io.connect({transports: ['websocket']});
 socket.on('connect', function (event) {
     name = prompt("Please Enter a Username", "Username");
-    /*while(isUsed(name)){
+    while(isUsed(name)){
         name = prompt("Please Enter Another Username", name);
-    };*/
+    };
     socket.emit("register", name)
 });
 socket.on('message', function (event) {
     // received a message from the server
     gameState = event.parse()
-});
+});*/
 
 
 //Username input, repeats if blank
